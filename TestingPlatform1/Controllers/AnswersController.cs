9c57c80586b628fc7f7fc209b05af7c5273d8047
Admin <Admin@DESKTOP-7ULD2OU>
2025-09-21 20:28:58 +0500

    Добавьте файлы проекта.

---
+++ TestingPlatform1/Controllers/AnswersController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestingPlatform1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AnswersController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetAllAnswers() => Ok("Список всех ответов");
+
+        [HttpGet("{id}")]
+        public IActionResult GetAnswerById(int id)
+        {
+            if (id == 1)
+                return Ok("Ответ 1");
+            return NotFound();
+        }
+
+        [HttpGet("by-question/{questionId}")]
+        public IActionResult GetAnswersByQuestionId(int QuestioId) =>
+          Ok($"Ответы на вопросы {QuestioId}");
+
+
+        [HttpPost]
+        public IActionResult CreateAnswer() => Created("/api/Answers/1", "Ответ создан");
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateAnswer(int id) => NoContent();
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteAnswer(int id) => NoContent();
+    }
+
+}
