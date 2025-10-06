0243638b0bde376941ff67d468b3c7d988023862
S1dDe15 <anisimovp201@gmail.com>
2025-10-06 20:22:04 +0500

    ....

--- TestingPlatform1/Controllers/StudentsController.cs
+++ TestingPlatform1/Controllers/StudentsController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TestingPlatform1;
+using TestingPlatform1.Models;
 
 namespace TestingPlatform.Controllers
 {
@@ -6,52 +8,87 @@ namespace TestingPlatform.Controllers
     [Route("api/[controller]")] // базовый маршрут: /api/students
     public class StudentsController : ControllerBase
     {
+        private readonly AppDbContext _db;
+
+        public StudentsController(AppDbContext db) 
+        {
+            _db = db;
+
+        }
+
         [HttpGet]
-        public IActionResult GetAllStudents() => Ok("Список студентов");
-
-
+        public IActionResult GetAllStudents() 
+        {
+           var students = _db.Students.ToList();                    
+           
+           return Ok(students);
+        }   
+                             
+                    
+        
         [HttpGet("{id:int}")]
         public IActionResult GetStudentById(int id)
         {
             if (id <= 0)
-                return BadRequest("Некорретный id");
-            if (id == 1)
-                return Ok("Студент 1");
-            return NotFound();
-        }
-
-        [HttpPost]
-        public IActionResult CreateStudent()
-        {
-            // Имитация: создали студента с id=1
-            return Created("/api/students/1", "Создан студент с id=1"); // 201 + Location
+                return BadRequest("Некорретный id"); //400
+            var students = _db.Students.FirstOrDefault(s  => s.Id == id);
+            if (students is null)
+                return NotFound();
+            return Ok(students);
            
         }
 
-        [HttpPut("{id:int}")]
-        public IActionResult UpdateStudent(int id)
+        [HttpPost]
+        public IActionResult CreateStudent([FromBody] Student student)
         {
-            if (id <= 0)
-                return BadRequest("Некорректный id"); // 400
-                                                      // Имитация: если не существует
-            if (id != 1)
-                return NotFound(); // 404
-                                   // Имитация: обновили
-            return NoContent(); // 204
+            var emailExists = _db.Students.Where(s => s.Email == student.Email).ToList();
+            if (emailExists.Any())
+                return Conflict("Такой email уже используется");
+
+            _db.Students.Add(student);
+            _db.SaveChanges();
+
+           return Created();
         }
 
-        [HttpDelete("{id:int}")]
+        [HttpPut("{id:int}")]
+        public IActionResult UpdateStudent([FromRoute] int id, [FromBody] Student student)
+
+        {
+
+            if (id != student.Id)
+                    return BadRequest("id в пути и в теле запроса не совпадают");
+
+                if (id <= 0)
+                    return BadRequest("Некорректный id");
+
+                var exists = _db.Students.Any(s => s.Id == id);
+                if (!exists)
+                    return NotFound();
+
+                var emailExists = _db.Students.FirstOrDefault(s => s.Email == student.Email && s.Id != id);
+                if (emailExists is not null)
+                    return Conflict("Такой email уже используется"); //409
+
+                _db.Students.Update(student);
+                _db.SaveChanges();
+
+                return NoContent();
+
+        }
+
+            [HttpDelete("{id:int}")]
         public IActionResult DeleteStudent(int id)
         {
-            if (id <= 0)
-                return BadRequest("Некорректный id"); // 400
-            if (id != 1) return NotFound(); // 404
-            return NoContent(); // 204
+            var student = _db.Students.Find(id);
+            if (student is null)
+                return NotFound();
+
+            _db.Students.Remove(student);
+            _db.SaveChanges();
+
+            return NoContent();
         }
-
-
-
-
 
     }
 }
